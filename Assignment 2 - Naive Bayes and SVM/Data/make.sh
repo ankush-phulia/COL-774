84d45fef12dcfde5261586bcb7fe053e60257e06
aphulia <ankush.phulia22@gmail.com>
2018-06-26 22:59:50 +0530

    Compress data folders

--- Assignment 2 - Naive Bayes and SVM/Data/make.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install python-dev
-sudo apt-get install python-pip
-sudo pip install numpy
-sudo pip install nltk
-sudo pip install stop_words
--- Assignment 2 - Naive Bayes and SVM/Data/svm/showface.m
+++
@@ -1,4 +0,0 @@
-function showface(face)
-figure;
-imagesc(reshape(face,86,86) * 255 * 1000);
-colormap(gray);
